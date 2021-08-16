aa13872f4309ff12cd105f0ad79b03a0430ff905
Eric Wu <6261790+ericwu119@users.noreply.github.com>
2021-08-16 14:56:17 -0700

    Finished Gatsby Tutorial Part 2

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,22 +1,12 @@
-// Import react.js
 import * as React from 'react'
-import {Link}  from 'gatsby'
-import Layout from "../components/layout"
-import Seo from "../components/seo" 
+import Layout from '../components/layout'
 
-// Define compoinent
 const AboutPage = () => {
     return (
-        <Layout>
-        <Seo title="About Me" />
-        <h1>About Me</h1>
-        <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
-        <p>
-          <Link to="/">Back to Home</Link>
-        </p>
-      </Layout>
+        <Layout pageTitle="About Me!">
+            <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+        </Layout>
     )
 }
 
-// export component
 export default AboutPage
